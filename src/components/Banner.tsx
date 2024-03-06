7a8f66e23d90d48abfb01d72ca6ce59190e3d35e
Louie Knolle <knollelw@gmail.com>
2024-03-06 15:09:10 -0500

    rearranges title above hero

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,6 +5,12 @@ export default function Banner() {
 	return (
 		<section className='w-full py-36' id='top' role='banner'>
 			<div className='container px-4 md:px-6 flex flex-col items-center gap-y-4'>
+				<div className='flex items-baseline gap-4'>
+					<h1 className='text-4xl font-bold tracking-tighter sm:text-5xl'>
+						Cotter Farm   
+					</h1>
+					<h3 className='text-3xl'>   Community Supported Agriculture</h3>
+				</div>
 				<img
 					alt='horizontal rows of crops in a greenhouse with a person in the background watering plants'
 					className='mx-auto aspect-video overflow-hidden rounded-xl object-cover object-center w-full'
@@ -12,10 +18,6 @@ export default function Banner() {
 					src={bannerImg}
 					width='600'
 				/>
-				<h1 className='text-4xl font-bold tracking-tighter sm:text-5xl'>
-					Cotter Farm
-				</h1>
-				<h3 className='text-3xl'>Community Supported Agriculture</h3>
 				<p className='mx-auto max-w-3xl text-gray-600 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed'>
 					Enjoy a weekly box of fresh, locally grown produce straight from
 					our farm to your table. Our CSA program connects you with the best
