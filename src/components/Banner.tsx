a0446e993527242e30b1aa81f030f4046e2a7a15
Louie Knolle <knollelw@gmail.com>
2024-04-17 12:35:06 -0400

    creates temporary sign up banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -12,11 +12,11 @@ export default function Banner() {
 					backgroundRepeat: 'no-repeat'
 				}}
 			>
-				<div className='flex w-full max-w-2xl flex-col items-center justify-center rounded-xl border-2 border-gray-950 bg-gray-200 bg-opacity-80 p-8 text-center font-bold'>
+				<div className='flex w-2/3 md:w-full max-w-2xl flex-col items-center justify-center rounded-xl border-2 border-gray-950 bg-gray-200 bg-opacity-80 p-8 text-center font-bold'>
 					<h1 className='text-4xl font-bold tracking-tighter sm:text-5xl'>
 						Cotter Farm CSA
 					</h1>
-					<p className='mx-auto max-w-3xl md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed'>
+					<p className='mx-auto max-w-3xl  md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed'>
 						Join our Community Supported Agriculture and enjoy a weekly box of fresh, locally grown produce straight from our farm to your table.
 					</p>
 				</div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@mui/material'
 import AboutUs from 'components/AboutUs'
 import Banner from 'components/Banner'
 import ContactForm from 'components/ContactForm'
@@ -10,6 +11,17 @@ export default function Component() {
 	return (
 		<div className='flex min-h-[100dvh] flex-col items-center justify-center'>
 			<Header />
+			<div className='bg-green-200 border-t-4 border-green-500 text-green-900 p-4 text-center shadow-2xl'>
+				<h3 className='text-3xl font-bold tracking-tighter sm:text-4xl'>
+					Sign up now for the 2024 season!
+				</h3>
+				<div className='mt-4 space-y-2'>
+					<p className='text-lg'>Full shares: <span className='font-semibold'>$400</span></p>
+					<p className='text-lg'>Half shares: <span className='font-semibold'>$200</span></p>
+					<p className='text-lg'>Delivery service(optional): <span className='font-semibold'>$200</span></p>
+				</div>
+				<Button className='mt-6' variant='contained' sx={{'backgroundColor': 'darkgreen', color:'white', '&:hover': {backgroundColor: 'darkgreen', color: 'white'}, boxShadow: 'revert'}}><a href='/#contact'>Get in touch</a></Button>
+			</div>
 			<Banner />
 			<main className='flex flex-1 flex-col items-center justify-center'>
 				<CSAInfo />
