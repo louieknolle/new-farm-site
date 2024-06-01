1e4461199c2215d8140e59cf2aff3095aa4fe0ab
Louie Knolle <knollelw@gmail.com>
2024-05-31 21:13:34 -0400

    remove 2024 sign up CTA

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -2,64 +2,66 @@ import { Button } from '@mui/material'
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export default function Component() {
 	return (
-		<section className='w-full py-36' id='contact' >
+		<section className='w-full py-36' id='contact'>
 			<div className='container grid items-center gap-6 px-4 md:px-6'>
 				<div className='mx-auto max-w-3xl space-y-4'>
 					<div className='space-y-2'>
 						<h2 className='text-3xl font-bold tracking-tighter sm:text-4xl'>
 							Contact Us or Sign Up!
 						</h2>
-						<p className='text-gray-600 md:text-xl/relaxed max-w-lg'>
-							Interested in joining as a CSA member? Want to learn more about
-							our farm? We would love to hear from you. Contact us using the
-							information below. Or call us directly at 513-707-8960.
+						<p className='max-w-lg text-gray-600 md:text-xl/relaxed'>
+							Want to learn more about our farm? We would love to hear from you.
+							Contact us using the form below. Or call us directly at
+							513-707-8960.
 						</p>
 					</div>
 					<form
 						action='https://api.web3forms.com/submit'
 						method='POST'
-						className='flex flex-col gap-4 w-full justify-center items-center'
+						className='flex w-full flex-col items-center justify-center gap-4'
 					>
 						<input
 							type='hidden'
 							name='access_key'
 							value='c0398686-ae2e-4de4-9139-19a616061139'
 						/>
-						<label htmlFor='name' className='flex flex-col w-full'>
+						<label htmlFor='name' className='flex w-full flex-col'>
 							Name
-							<input 
+							<input
 								placeholder='Myst Rainwater'
 								type='text'
 								aria-label='name-input'
 								required
-								name='name' 
+								name='name'
 								id='name'
 							/>
 						</label>
-						<label htmlFor='email' className='flex flex-col w-full'>
+						<label htmlFor='email' className='flex w-full flex-col'>
 							Email
-							<input 
+							<input
 								placeholder='hello@email.com'
 								type='text'
 								aria-label='email-input'
 								required
-								name='email' 
+								name='email'
 								id='email'
 							/>
 						</label>
-						<label htmlFor='message' className='flex flex-col w-full'>
+						<label htmlFor='message' className='flex w-full flex-col'>
 							Message
-							<textarea 
-								placeholder='Hello, I am interested in joining your CSA!'								
+							<textarea
+								placeholder="Hello, is it too early to sign up for next year? I'm interested in a full share and delivery service. Thanks"
 								aria-label='message-input'
 								required
 								name='message'
 								id='message'
-								rows={4} 
+								rows={4}
 							/>
 						</label>
 						<input type='checkbox' name='botcheck' className='hidden' />
-						<Button variant='contained' sx={{width: '50%'}} type='submit'>Submit</Button>
+						<Button variant='contained' sx={{ width: '50%' }} type='submit'>
+							Submit
+						</Button>
 					</form>
 				</div>
 			</div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@mui/material'
+// import { Button } from '@mui/material'
 import AboutUs from 'components/AboutUs'
 import Banner from 'components/Banner'
 import ContactForm from 'components/ContactForm'
@@ -11,7 +11,7 @@ export default function Component() {
 	return (
 		<div className='flex min-h-[100dvh] flex-col items-center justify-center'>
 			<Header />
-			<div className='bg-green-200 border-t-4 border-green-500 text-green-900 p-4 text-center shadow-2xl'>
+			{/* <div className='bg-green-200 border-t-4 border-green-500 text-green-900 p-4 text-center shadow-2xl'>
 				<h3 className='text-3xl font-bold tracking-tighter sm:text-4xl'>
 					Sign up now for the 2024 season!
 				</h3>
@@ -21,7 +21,7 @@ export default function Component() {
 					<p className='text-lg'>Delivery service(optional): <span className='font-semibold'>$200</span></p>
 				</div>
 				<Button className='mt-6' variant='contained' sx={{'backgroundColor': 'darkgreen', color:'white', '&:hover': {backgroundColor: 'darkgreen', color: 'white'}, boxShadow: 'revert'}}><a href='/#contact'>Get in touch</a></Button>
-			</div>
+			</div> */}
 			<Banner />
 			<main className='flex flex-1 flex-col items-center justify-center'>
 				<CSAInfo />
